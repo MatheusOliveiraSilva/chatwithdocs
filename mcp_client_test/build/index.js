281790d4b9bf09212a1fc4d1a58e76d89406529f
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-05-25 15:15:13 -0300

    mcp testing

---
+++ mcp_client_test/build/index.js
@@ -0,0 +1,53 @@
+import { Anthropic } from "@anthropic-ai/sdk";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import dotenv from "dotenv";
+dotenv.config();
+const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
+if (!ANTHROPIC_API_KEY) {
+    throw new Error("ANTHROPIC_API_KEY is not set");
+}
+class MCPClient {
+    mcp;
+    anthropic;
+    transport = null;
+    tools = [];
+    constructor() {
+        this.anthropic = new Anthropic({
+            apiKey: ANTHROPIC_API_KEY,
+        });
+        this.mcp = new Client({ name: "mcp-client-cli", version: "1.0.0" });
+    }
+    async connectToServer(serverScriptPath) {
+        try {
+            const isJs = serverScriptPath.endsWith(".js");
+            const isPy = serverScriptPath.endsWith(".py");
+            if (!isJs && !isPy) {
+                throw new Error("Server script must be a .js or .py file");
+            }
+            const command = isPy
+                ? process.platform === "win32"
+                    ? "python"
+                    : "python3"
+                : process.execPath;
+            this.transport = new StdioClientTransport({
+                command,
+                args: [serverScriptPath],
+            });
+            this.mcp.connect(this.transport);
+            const toolsResult = await this.mcp.listTools();
+            this.tools = toolsResult.tools.map((tool) => {
+                return {
+                    name: tool.name,
+                    description: tool.description,
+                    input_schema: tool.inputSchema,
+                };
+            });
+            console.log("Connected to server with tools:", this.tools.map(({ name }) => name));
+        }
+        catch (e) {
+            console.log("Failed to connect to MCP server: ", e);
+            throw e;
+        }
+    }
+}
